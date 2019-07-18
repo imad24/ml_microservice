95c38fa434942ebc4ec662a6d554da88246a0c7f
Imad RAHMOUNI <rahmounimad@gmail.com>
2019-07-18 10:34:15 +0200

    fix server import

--- {{cookiecutter.project_name}}/controllers/server.py
+++ {{cookiecutter.project_name}}/controllers/server.py
@@ -1,9 +1,9 @@
 import os
 from waitress import serve
-from train_app import app
+from app import app
 
 if __name__ == "__main__":
-    app.debug = True
+    app.debug = False
     server = os.getenv("SERVER")
     port = os.getenv("PORT")
     serve(app, host=server, port=port)
